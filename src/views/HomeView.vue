5996c01fb6c4e2741eea055ed6e3271c507bf4b6
leowitcroz <leowitcroz@gmail.com>
2023-10-02 22:21:35 -0300

    sideBar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,15 @@
 <template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-2 sidebar">
+        <div class="link-box" style="margin-top: 15%; margin-left: 2%;">
+          <p class="links">Adicionar Pokemon</p>
+          <p class="links">Sua Pokedex</p>
+          <p class="links">Lista de pokemons</p>
+        </div>
+      </div>
 
-  <div class="container-fluid" style="display: flex;">
-
-    <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block sidebar" style="height: 100vh; background-color: black;">
-                <div class="position-sticky">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">
-                                Dashboard
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                Orders
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                Products
-                            </a>
-                        </li>
-                        <!-- Add more sidebar items as needed -->
-                    </ul>
-                </div>
-            </nav>
-
-      <div class="col-lg-9">
+      <div class="col-lg-10" style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
         <h1>POKEDEX</h1>
 
         <input type="text" v-model="pokeName">
@@ -33,13 +17,9 @@
         <button @click="searchPokemon(pokeName);">Click</button>
 
         <h1>{{ pokeChosen }}</h1>
-
       </div>
-  
+    </div>
   </div>
-
-
-  
 </template>
 
 <script lang="ts" setup>
@@ -52,10 +32,10 @@ let pokeName = ref()
 
 let pokeChosen = ref()
 
-let searchPokemon = (name: string) =>{
+let searchPokemon = (name: string) => {
   console.log(pokemon)
-  for(let i = 0; i < pokemon.length; i++){
-    if(pokemon[i].name == name){
+  for (let i = 0; i < pokemon.length; i++) {
+    if (pokemon[i].name == name) {
       console.log(pokemon[i])
       pokeChosen.value = pokemon[i].name
     }
@@ -63,10 +43,8 @@ let searchPokemon = (name: string) =>{
   return null
 }
 
-
-
 onMounted(() => {
-  fetch('https://pokeapi.co/api/v2/pokemon?limit=1292').then(res => res.json()).then(res => {pokemon = res.results; console.log(pokemon)})
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=1292').then(res => res.json()).then(res => { pokemon = res.results; console.log(pokemon) })
 })
 
 
@@ -74,10 +52,31 @@ onMounted(() => {
 
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Pixelify+Sans&display=swap');
+
 * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box; /* Isso garante que o tamanho total de um elemento inclua a borda e o preenchimento, não apenas o conteúdo */
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  /* Isso garante que o tamanho total de um elemento inclua a borda e o preenchimento, não apenas o conteúdo */
 }
 
+.sidebar{
+  height: 100vh;
+  background-color:rgb(80, 179, 249) ;
+}
+
+.links{
+  font-family: 'Pixelify Sans', cursive;
+  font-size: 22px !important;
+  color: yellow;
+  font-weight: 500;
+  margin-top: 5%;
+}
+
+.links:hover{
+  color: red;
+  cursor: pointer;
+  text-decoration: underline;
+}
 </style>
